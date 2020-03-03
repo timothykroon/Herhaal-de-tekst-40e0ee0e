cca71455383bc2cfde1e747008942108768c1f27
Timothy Kroon <timothykroon1@gmail.com>
2020-03-03 10:25:56 +0100

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,8 @@
+<?php
+    $tekst = "Tekst in een variable";
+    echo $tekst . PHP_EOL;
+    echo $tekst . PHP_EOL;
+    echo $tekst . PHP_EOL;
+    echo $tekst . PHP_EOL;
+    echo $tekst . PHP_EOL;
+?>
